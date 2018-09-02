047545a61115271ed0908d43aca276af331f3b0b
Eric Binnion <ericbinnion@gmail.com>
2018-09-02 12:56:35 -0500

    Bust cache for stylesheet

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_eric_blog_ixion_scripts', 11 );
 
 function enqueue_eric_blog_ixion_scripts() {
 	wp_enqueue_style( 'eric-blog-ixion-child', get_template_directory_uri() . '/style.css', array(), '20151215' );
-	wp_enqueue_style( 'eric-blog', get_stylesheet_uri(), array( 'eric-blog-ixion-child' ), '20180902' );
+	wp_enqueue_style( 'eric-blog', get_stylesheet_uri(), array( 'eric-blog-ixion-child' ), '20180902-1' );
 
 	wp_dequeue_style( 'ixion-fonts-archivo' );
 	wp_dequeue_style( 'ixion-fonts-cooper' );
