ec9f9fcdc86ee96458bc82a063d6f00eb5641454
Eric Binnion <ericbinnion@gmail.com>
2018-09-02 13:07:11 -0500

    Use jQuery 3 on frontend

--- functions.php
+++ functions.php
@@ -9,4 +9,10 @@ function enqueue_eric_blog_ixion_scripts() {
 	wp_dequeue_style( 'ixion-fonts-archivo' );
 	wp_dequeue_style( 'ixion-fonts-cooper' );
 	wp_dequeue_style( 'genericons' );
+
+	// Use jQuery 3.
+	wp_deregister_script( 'jquery-core' );
+	wp_register_script( 'jquery-core', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), '3.3.1' );
+	wp_deregister_script( 'jquery-migrate' );
+	wp_register_script( 'jquery-migrate', 'https://code.jquery.com/jquery-migrate-3.0.0.min.js', array(), '3.0.0' );
 }
